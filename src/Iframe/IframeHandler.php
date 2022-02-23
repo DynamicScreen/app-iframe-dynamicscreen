b15db4040ccdff7704c208829da6804e51b65fbb
Ngras <nicolas.g33@hotmail.fr>
2022-02-23 16:00:57 +0100

    wip update config

---
+++ src/Iframe/IframeHandler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DynamicScreen\Iframe\Iframe;
+
+use DynamicScreen\SdkPhp\Handlers\SlideHandler;
+use DynamicScreen\SdkPhp\Interfaces\ISlide;
+
+class SimpleMessageHandler extends SlideHandler 
+{
+    
+}
