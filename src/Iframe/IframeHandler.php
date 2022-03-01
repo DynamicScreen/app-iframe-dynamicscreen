5933c96951880752217d0a5f4b5127bd11a797b2
Ngras <nicolas.g33@hotmail.fr>
2022-03-01 12:26:57 +0100

    iframe wip

--- src/Iframe/IframeHandler.php
+++ src/Iframe/IframeHandler.php
@@ -4,8 +4,16 @@ namespace DynamicScreen\Iframe\Iframe;
 
 use DynamicScreen\SdkPhp\Handlers\SlideHandler;
 use DynamicScreen\SdkPhp\Interfaces\ISlide;
+use Illuminate\Support\Arr;
 
-class SimpleMessageHandler extends SlideHandler 
+class IframeHandler extends SlideHandler
 {
-    
+
+    public function fetch(ISlide $slide): void
+    {
+        $this->addSlide([
+            'url' => $slide->getOption('url', ''),
+            'refresh_content' => $slide->getOption('refresh_content', 'false')
+        ]);
+    }
 }
--- src/Iframe/IframeOptions.ts
+++ src/Iframe/IframeOptions.ts
@@ -1,29 +1,49 @@
 
-   
 import {
     ISlideOptionsContext,
-    SlideOptionsModule, VueInstance
+    SlideOptionsModule, 
+    VueInstance
   } from "dynamicscreen-sdk-js";
+import debounce from "debounce";
+import axios from "axios";
+
 
 export default class IframeOptionsModule extends SlideOptionsModule {
-    constructor(context: ISlideOptionsContext) {
-        super(context);
-    }
-
     async onReady() {
         return true;
     };
 
     // @ts-ignore
     setup(props: Record<string, any>, vue: VueInstance, context: ISlideOptionsContext) {
-        const { h } = vue;
+        //@ts-ignore
+        const { h, ref, reactive, watch, toRefs, toRef, computed } = vue;
 
         const update = context.update;
 
-        const { Field, TextInput } = this.context.components
+        const { Field, TextInput, Toggle } = this.context.components
 
-        console.log('in setup before return h')
+        const url = toRef(props.modelValue, 'url') || ref<string>('');
+        const isUrlValid = ref(true);
+
+        watch(() => url.value, debounce((value) => {
+            axios.get(value).then((response) => {
+                isUrlValid.value = true;
+            }).catch((err) => {
+                console.log("ERROR", err)
+                isUrlValid.value = false;
+            })
+        }, 500))
+
         return () =>
-            h("div", {}, [])
+            h("div", { class: 'space-y-4' }, [
+                h(Field, { label: this.t('modules.iframe.options.url') }, [
+                    h(TextInput, {...update.option("url") }),
+                    h("div", { class: isUrlValid.value ? "invisible" : "visible text-red-500 text-sm" }, this.t('modules.iframe.options.invalid_url'))
+                ]),
+                
+                h(Field, {}, [
+                    h(Toggle, { class: 'flex-1', ...update.option("refresh_content") }, this.t('modules.iframe.options.refresh_content'))
+                ]),
+            ])
     }
 }
